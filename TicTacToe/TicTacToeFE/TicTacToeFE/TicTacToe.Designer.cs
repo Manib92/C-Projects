a28975649b64a047d362604080addfa067ffbd60
Manib92 <42023799+Manib92@users.noreply.github.com>
2020-12-16 15:21:26 +0000

    Tic Tac Toe

---
+++ TicTacToe/TicTacToeFE/TicTacToeFE/TicTacToe.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace TicTacToeFE
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TicTacToe));
+            this.TopLeft = new System.Windows.Forms.Button();
+            this.TopCenter = new System.Windows.Forms.Button();
+            this.TopRight = new System.Windows.Forms.Button();
+            this.CenterLeft = new System.Windows.Forms.Button();
+            this.CenterCenter = new System.Windows.Forms.Button();
+            this.CenterRight = new System.Windows.Forms.Button();
+            this.BottomLeft = new System.Windows.Forms.Button();
+            this.BottomCenter = new System.Windows.Forms.Button();
+            this.BottomRight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TopLeft
+            // 
+            this.TopLeft.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TopLeft.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.TopLeft.FlatAppearance.BorderSize = 3;
+            this.TopLeft.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.TopLeft.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.TopLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TopLeft.Font = new System.Drawing.Font("Segoe Print", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TopLeft.ForeColor = System.Drawing.Color.Blue;
+            this.TopLeft.Location = new System.Drawing.Point(0, 0);
+            this.TopLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.TopLeft.Name = "TopLeft";
+            this.TopLeft.Size = new System.Drawing.Size(200, 200);
+            this.TopLeft.TabIndex = 0;
+            this.TopLeft.UseVisualStyleBackColor = false;
+            this.TopLeft.Click += new System.EventHandler(this.ButtonClick);
+            this.TopLeft.MouseLeave += new System.EventHandler(this.ButtonLeave);
+            this.TopLeft.MouseHover += new System.EventHandler(this.ButtonHover);
+            // 
+            // TopCenter
+            // 
+            this.TopCenter.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TopCenter.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.TopCenter.FlatAppearance.BorderSize = 3;
+            this.TopCenter.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.TopCenter.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.TopCenter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TopCenter.Font = new System.Drawing.Font("Segoe Print", 60F, System.Drawing.FontStyle.Bold);
+            this.TopCenter.ForeColor = System.Drawing.Color.Blue;
+            this.TopCenter.Location = new System.Drawing.Point(200, 0);
+            this.TopCenter.Margin = new System.Windows.Forms.Padding(0);
+            this.TopCenter.Name = "TopCenter";
+            this.TopCenter.Size = new System.Drawing.Size(200, 200);
+            this.TopCenter.TabIndex = 1;
+            this.TopCenter.UseVisualStyleBackColor = false;
+            this.TopCenter.Click += new System.EventHandler(this.ButtonClick);
+            this.TopCenter.MouseLeave += new System.EventHandler(this.ButtonLeave);
+            this.TopCenter.MouseHover += new System.EventHandler(this.ButtonHover);
+            // 
+            // TopRight
+            // 
+            this.TopRight.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TopRight.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.TopRight.FlatAppearance.BorderSize = 3;
+            this.TopRight.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.TopRight.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.TopRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TopRight.Font = new System.Drawing.Font("Segoe Print", 60F, System.Drawing.FontStyle.Bold);
+            this.TopRight.ForeColor = System.Drawing.Color.Blue;
+            this.TopRight.Location = new System.Drawing.Point(400, 0);
+            this.TopRight.Margin = new System.Windows.Forms.Padding(0);
+            this.TopRight.Name = "TopRight";
+            this.TopRight.Size = new System.Drawing.Size(200, 200);
+            this.TopRight.TabIndex = 2;
+            this.TopRight.UseVisualStyleBackColor = false;
+            this.TopRight.Click += new System.EventHandler(this.ButtonClick);
+            this.TopRight.MouseLeave += new System.EventHandler(this.ButtonLeave);
+            this.TopRight.MouseHover += new System.EventHandler(this.ButtonHover);
+            // 
+            // CenterLeft
+            // 
+            this.CenterLeft.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CenterLeft.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.CenterLeft.FlatAppearance.BorderSize = 3;
+            this.CenterLeft.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.CenterLeft.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.CenterLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CenterLeft.Font = new System.Drawing.Font("Segoe Print", 60F, System.Drawing.FontStyle.Bold);
+            this.CenterLeft.ForeColor = System.Drawing.Color.Blue;
+            this.CenterLeft.Location = new System.Drawing.Point(0, 200);
+            this.CenterLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.CenterLeft.Name = "CenterLeft";
+            this.CenterLeft.Size = new System.Drawing.Size(200, 200);
+            this.CenterLeft.TabIndex = 3;
+            this.CenterLeft.UseVisualStyleBackColor = false;
+            this.CenterLeft.Click += new System.EventHandler(this.ButtonClick);
+            this.CenterLeft.MouseLeave += new System.EventHandler(this.ButtonLeave);
+            this.CenterLeft.MouseHover += new System.EventHandler(this.ButtonHover);
+            // 
+            // CenterCenter
+            // 
+            this.CenterCenter.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CenterCenter.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.CenterCenter.FlatAppearance.BorderSize = 3;
+            this.CenterCenter.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.CenterCenter.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.CenterCenter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CenterCenter.Font = new System.Drawing.Font("Segoe Print", 60F, System.Drawing.FontStyle.Bold);
+            this.CenterCenter.ForeColor = System.Drawing.Color.Blue;
+            this.CenterCenter.Location = new System.Drawing.Point(200, 200);
+            this.CenterCenter.Margin = new System.Windows.Forms.Padding(0);
+            this.CenterCenter.Name = "CenterCenter";
+            this.CenterCenter.Size = new System.Drawing.Size(200, 200);
+            this.CenterCenter.TabIndex = 4;
+            this.CenterCenter.UseVisualStyleBackColor = false;
+            this.CenterCenter.Click += new System.EventHandler(this.ButtonClick);
+            this.CenterCenter.MouseLeave += new System.EventHandler(this.ButtonLeave);
+            this.CenterCenter.MouseHover += new System.EventHandler(this.ButtonHover);
+            // 
+            // CenterRight
+            // 
+            this.CenterRight.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CenterRight.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.CenterRight.FlatAppearance.BorderSize = 3;
+            this.CenterRight.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.CenterRight.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.CenterRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CenterRight.Font = new System.Drawing.Font("Segoe Print", 60F, System.Drawing.FontStyle.Bold);
+            this.CenterRight.ForeColor = System.Drawing.Color.Blue;
+            this.CenterRight.Location = new System.Drawing.Point(400, 200);
+            this.CenterRight.Margin = new System.Windows.Forms.Padding(0);
+            this.CenterRight.Name = "CenterRight";
+            this.CenterRight.Size = new System.Drawing.Size(200, 200);
+            this.CenterRight.TabIndex = 5;
+            this.CenterRight.UseVisualStyleBackColor = false;
+            this.CenterRight.Click += new System.EventHandler(this.ButtonClick);
+            this.CenterRight.MouseLeave += new System.EventHandler(this.ButtonLeave);
+            this.CenterRight.MouseHover += new System.EventHandler(this.ButtonHover);
+            // 
+            // BottomLeft
+            // 
+            this.BottomLeft.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BottomLeft.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BottomLeft.FlatAppearance.BorderSize = 3;
+            this.BottomLeft.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.BottomLeft.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.BottomLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BottomLeft.Font = new System.Drawing.Font("Segoe Print", 60F, System.Drawing.FontStyle.Bold);
+            this.BottomLeft.ForeColor = System.Drawing.Color.Blue;
+            this.BottomLeft.Location = new System.Drawing.Point(0, 400);
+            this.BottomLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.BottomLeft.Name = "BottomLeft";
+            this.BottomLeft.Size = new System.Drawing.Size(200, 200);
+            this.BottomLeft.TabIndex = 6;
+            this.BottomLeft.UseVisualStyleBackColor = false;
+            this.BottomLeft.Click += new System.EventHandler(this.ButtonClick);
+            this.BottomLeft.MouseLeave += new System.EventHandler(this.ButtonLeave);
+            this.BottomLeft.MouseHover += new System.EventHandler(this.ButtonHover);
+            // 
+            // BottomCenter
+            // 
+            this.BottomCenter.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BottomCenter.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BottomCenter.FlatAppearance.BorderSize = 3;
+            this.BottomCenter.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.BottomCenter.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.BottomCenter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BottomCenter.Font = new System.Drawing.Font("Segoe Print", 60F, System.Drawing.FontStyle.Bold);
+            this.BottomCenter.ForeColor = System.Drawing.Color.Blue;
+            this.BottomCenter.Location = new System.Drawing.Point(200, 400);
+            this.BottomCenter.Margin = new System.Windows.Forms.Padding(0);
+            this.BottomCenter.Name = "BottomCenter";
+            this.BottomCenter.Size = new System.Drawing.Size(200, 200);
+            this.BottomCenter.TabIndex = 7;
+            this.BottomCenter.UseVisualStyleBackColor = false;
+            this.BottomCenter.Click += new System.EventHandler(this.ButtonClick);
+            this.BottomCenter.MouseLeave += new System.EventHandler(this.ButtonLeave);
+            this.BottomCenter.MouseHover += new System.EventHandler(this.ButtonHover);
+            // 
+            // BottomRight
+            // 
+            this.BottomRight.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BottomRight.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BottomRight.FlatAppearance.BorderSize = 3;
+            this.BottomRight.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.BottomRight.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.BottomRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BottomRight.Font = new System.Drawing.Font("Segoe Print", 60F, System.Drawing.FontStyle.Bold);
+            this.BottomRight.ForeColor = System.Drawing.Color.Blue;
+            this.BottomRight.Location = new System.Drawing.Point(400, 400);
+            this.BottomRight.Margin = new System.Windows.Forms.Padding(0);
+            this.BottomRight.Name = "BottomRight";
+            this.BottomRight.Size = new System.Drawing.Size(200, 200);
+            this.BottomRight.TabIndex = 8;
+            this.BottomRight.UseVisualStyleBackColor = false;
+            this.BottomRight.Click += new System.EventHandler(this.ButtonClick);
+            this.BottomRight.MouseLeave += new System.EventHandler(this.ButtonLeave);
+            this.BottomRight.MouseHover += new System.EventHandler(this.ButtonHover);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.BottomRight);
+            this.Controls.Add(this.BottomCenter);
+            this.Controls.Add(this.BottomLeft);
+            this.Controls.Add(this.CenterRight);
+            this.Controls.Add(this.CenterCenter);
+            this.Controls.Add(this.CenterLeft);
+            this.Controls.Add(this.TopRight);
+            this.Controls.Add(this.TopCenter);
+            this.Controls.Add(this.TopLeft);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TicTacToe";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button CenterLeft;
+        private System.Windows.Forms.Button CenterCenter;
+        private System.Windows.Forms.Button CenterRight;
+        private System.Windows.Forms.Button BottomLeft;
+        private System.Windows.Forms.Button BottomCenter;
+        private System.Windows.Forms.Button BottomRight;
+        private System.Windows.Forms.Button TopLeft;
+        private System.Windows.Forms.Button TopCenter;
+        private System.Windows.Forms.Button TopRight;
+    }
+}
+
---
+++ TicTacToe/TicTacToeFE/TicTacToeFE/TicTacToe.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeFE
+{
+    public partial class TicTacToe : Form
+    {
+        public static string turn = "X";
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private static bool IsFree(object sender, EventArgs e)
+        {
+            //Check if button slot is free
+            var button = (Button)sender;
+            if(button.Text == "" || button.ForeColor == Color.Gray)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void SetMove(object sender, EventArgs e)
+        {
+            //Set button to either X or O when clicked
+            var button = (Button)sender;
+            if (turn == "X")
+            {
+                button.ForeColor = Color.Blue;
+                button.Text = turn;
+                turn = "O";
+            }
+            else
+            {
+                button.ForeColor = Color.Red;
+                button.Text = turn;
+                turn = "X";
+            }
+        }
+
+        private void IsGameOver()
+        {
+            //Check for game over conditions for the buttons.
+            List<Button> buttons = new List<Button> 
+            { TopLeft, TopCenter, TopRight, CenterLeft, CenterCenter,
+            CenterRight, BottomLeft, BottomCenter, BottomRight };
+            DialogResult result = new DialogResult();
+            // TODO - Improvement - get rid of checking all win cases when all files are filled. Just check for draw
+            //when all tiles are filled. and always check for possible winners later regardless of tiles being all 
+            //filled.
+
+            //Check for all buttons regardless if tiles are not filled.
+            if (buttons[0].Text == buttons[1].Text && buttons[0].Text == buttons[2].Text && buttons[0].Text != "")
+            {
+                result = MessageBox.Show($"The winner is {buttons[0].Text}! Would you like to reset?", "Game Over", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Reset();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            else if (buttons[3].Text == buttons[4].Text && buttons[3].Text == buttons[5].Text && buttons[3].Text != "")
+            {
+                result = MessageBox.Show($"The winner is {buttons[3].Text}! Would you like to reset?", "Game Over", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Reset();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            else if (buttons[6].Text == buttons[7].Text && buttons[6].Text == buttons[8].Text && buttons[6].Text != "")
+            {
+                result = MessageBox.Show($"The winner is {buttons[6].Text}! Would you like to reset?", "Game Over", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Reset();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            else if (buttons[0].Text == buttons[3].Text && buttons[0].Text == buttons[6].Text && buttons[0].Text != "")
+            {
+                result = MessageBox.Show($"The winner is {buttons[0].Text}! Would you like to reset?", "Game Over", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Reset();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            else if (buttons[1].Text == buttons[4].Text && buttons[1].Text == buttons[7].Text && buttons[1].Text != "")
+            {
+                result = MessageBox.Show($"The winner is {buttons[1].Text}! Would you like to reset?", "Game Over", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Reset();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            else if (buttons[2].Text == buttons[5].Text && buttons[2].Text == buttons[8].Text && buttons[2].Text != "")
+            {
+                result = MessageBox.Show($"The winner is {buttons[2].Text}! Would you like to reset?", "Game Over", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Reset();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            else if (buttons[0].Text == buttons[4].Text && buttons[0].Text == buttons[8].Text && buttons[0].Text != "")
+            {
+                result = MessageBox.Show($"The winner is {buttons[0].Text}! Would you like to reset?", "Game Over", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Reset();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            else if (buttons[2].Text == buttons[4].Text && buttons[2].Text == buttons[6].Text && buttons[2].Text != "")
+            {
+                result = MessageBox.Show($"The winner is {buttons[2].Text}! Would you like to reset?", "Game Over", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Reset();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            //Check when no more available moves are possible
+            else if (buttons.All(x => x.Text != ""))
+            {
+                result = MessageBox.Show("It was a tie game. Would you like to reset? ", "Game Over", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    Reset();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        private void Reset()
+        {
+            // Clears the input in the buttons
+            List<Button> buttons = new List<Button>
+            { TopRight, TopCenter, TopLeft, CenterLeft, CenterCenter,
+            CenterRight, BottomLeft, BottomCenter, BottomRight };
+
+            foreach(Button button in buttons)
+            {
+                button.Text = "";
+                turn = "X";
+            }
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            //Checks if button is free. If so, then it sets the move and checks for win condition.
+            Button button = (Button)sender;
+            if(IsFree(sender, e))
+            {
+                SetMove(sender, e);
+                IsGameOver();
+            }
+
+        }
+
+        private void ButtonHover(object sender, EventArgs e)
+        {
+            //If the button is free then it places an X or O there just to help the user visualize their move
+            if(IsFree(sender, e))
+            {
+                Button button = (Button)sender;
+                button.ForeColor = Color.Gray;
+                if (turn == "X")
+                {
+                    button.Text = "X";
+                }
+                else
+                {
+                    button.Text = "O";
+                }
+            }
+        }
+
+        private void ButtonLeave(object sender, EventArgs e)
+        {
+            //Clears the button of the "pencil" input made by ButtonHover when the mouse leaves that button.
+            if (IsFree(sender, e))
+            {
+                Button button = (Button)sender;
+                button.Text = "";
+            }
+        }
+    }
+}
